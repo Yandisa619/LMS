9158a98e63c9ee3252039abb3620b8fe49c2de6b
Yandisa619 <yndubela07@gmail.com>
2025-05-07 20:38:52 +0200

    Updated User Schema

--- server/models/User.js
+++ server/models/User.js
@@ -3,18 +3,19 @@ import mongoose from 'mongoose'
 // User Schema
 const userSchema = new mongoose.Schema(
     {
-        _id: {type: String, required: true},
-        name: {type: String, required: true},
-        email: {type: String, required: true},
-        imageUrl: {type: String, required: true},
+        _id: { type: String, required: true },
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        imageUrl: { type: String, required: true },
         enrolledCourses: [
             {
-                type: mongoose.SchemaType.ObjectId,
+                type: mongoose.Schema.Types.ObjectId, 
                 ref: 'Course'
             }
         ],
-    }, {timestamps: true});
+    }, 
+    { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
-
-export default User
+export default User;
